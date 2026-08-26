dd369791d6881e64dc0ebf867ec8359d5c37fc7f
Adarsh Modh <adarsh.modh@gmail.com>
2026-08-26 19:49:10 -0400

    Add TransformerBlock_Multi_Single_HeadAttention.py

--- TransformerBlock_Multi_Single_HeadAttention.py
+++ TransformerBlock_Multi_Single_HeadAttention.py
@@ -3,6 +3,29 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchtyping import TensorType
 
+class TransformerBlock(nn.Module):
+    def __init__(self, model_dim: int, num_heads: int):
+        super().__init__()
+        torch.manual_seed(0)
+        # Instantiate in this order:
+        # 1. self.MultiHeadedSelfAttention(model_dim, num_heads)
+        # 2. self.VanillaNeuralNetwork(model_dim)
+        # 3. Two nn.LayerNorm(model_dim) instances
+        self.attention = MultiHeadedSelfAttention(model_dim, num_heads)
+        self.linear_network = VanillaNeuralNetwork(model_dim)
+        self.first_norm = nn.LayerNorm(model_dim)
+        self.second_norm = nn.LayerNorm(model_dim)
+
+    def forward(self, embedded: TensorType[float]) -> TensorType[float]:
+        torch.manual_seed(0)
+        # Two residual connections with Pre-LN:
+        #   x = x + attention(layer_norm_1(x))
+        #   x = x + feed_forward(layer_norm_2(x))
+        # Return result rounded to 4 decimal places
+        embedded = embedded + self.attention(self.first_norm(embedded)) # skip connection
+        embedded = embedded + self.linear_network(self.second_norm(embedded)) # another skip connection
+        return torch.round(embedded, decimals=4)
+
 class MultiHeadedSelfAttention(nn.Module):
 
     def __init__(self, embedding_dim: int, attention_dim: int, num_heads: int):
@@ -59,3 +82,17 @@ class SingleHeadAttention(nn.Module):
         attn = scores @ V
 
         return attn
+
+class VanillaNeuralNetwork(nn.Module):
+        def __init__(self, model_dim: int):
+            super().__init__()
+            torch.manual_seed(0)
+            self.up_projection = nn.Linear(model_dim, model_dim * 4)
+            self.relu = nn.ReLU()
+            self.down_projection = nn.Linear(model_dim * 4, model_dim)
+            self.dropout = nn.Dropout(0.2) # using p = 0.2
+
+        def forward(self, x: TensorType[float]) -> TensorType[float]:
+            torch.manual_seed(0)
+            return self.dropout(self.down_projection(self.relu(self.up_projection(x))))
+
